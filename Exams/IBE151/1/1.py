91cba30c117654bd649b5d4c7bcde4ee676598a6
emilbratt <emilbratt@gmail.com>
2021-01-12 19:12:26 +0100

    exam is moved over to: "https://github.com/emilbratt/EDU/tree/master/Exams"

--- Exams/IBE151/1/1.py
+++
@@ -1,7 +0,0 @@
-
-
-# cast input from str to float and store number in miles
-miles = float(input('Enter the distance in miles: '))
-
-# we can just do the math operation inside the print statement
-print(str(miles*1609.344)+' m')
